91d398060e599cd123ff407b67316a80ef172686
karadoel <76063189+karadoel@users.noreply.github.com>
2020-12-15 18:31:12 -0800

    Create customer qs

---
+++ datacust.js
@@ -0,0 +1,32 @@
+// Questions
+var q = [
+	'Can you describe your offer?', 
+  'What are the top 3 benefits?', 
+  'What makes your offer different?', 
+  'Have you attracted other customers in my industry?', 
+  'How do you ensure reliability?', 
+  'How do you ensure my company's data is secure?', 
+  'How does your product integrate with other systems my company uses?', 
+  'Does the product require user training?', 
+  'How is pricing structured? Are there fees for support or upgrades?', 
+  'Who is your best competitor and why shouldn't we go with their offer?', 
+  'What have you learned from POCs?', 
+  'What challenges do you have?', 
+  'What are the response times for customer issues or bug fixes?', 
+  'What new features will you build in the future?', 
+  'How soon can we sign up?', 
+  
+
+	
+];
+
+// Tips
+var tips = [
+	'Try to have quick and concise answers.',
+	'Try to answer in 2 to 3 sentences.',
+	'Answer like you would in a conversation, this is not a presentation.',
+	'When talking to a potential customer, be substantive, and get straight to the point.',
+	'DO NOT BEAT AROUND THE BUSH. Do not hem and haw. "Uhm"s and "uh"s don&#39;t lend an aura of intelligence.',
+	
+	
+];
