c3c9609b3d0b59660f1922c41182802a6c067332
Kara Doelling <76063189+karadoel@users.noreply.github.com>
2021-01-05 10:00:58 -0800

    Create sailepractice.js

---
+++ sailepractice.js
@@ -0,0 +1,25 @@
+// Questions
+var q = [
+	'What is the vision?',
+  'Why don&#39;t others do this already?',
+  'What keeps others from being fast followers?',
+  'Do you know how much money you could save an owner of offshore assets?',
+  'What is the revenue model (i.e. pay per use or recurring revenue)?',
+  'To increase strategic value, what are potential synergies with RC&S sustainability strategy?',
+  'How does market size for oil and gas compare to other sectors you want to grow into, like offshore wind?',
+  'What are customer value levers (e.g. price, scheduling, flexibility, carbon reduction)?',
+  'What have you learned from other companies or pilots (such as Beyond Limits shipping and tracking POC)?',
+  'Will net zero ambition from day 1 be required?',
+  'How does your product work in more detail?',
+	'What is Saile&#39;s differentiator?',
+  'What challenges/obstacles do you face?',
+	'What are the switching costs for customers to use Saile?',
+  'What new features will you build after your first launch?', 
+];
+
+// Tips
+var tips = [
+	'Try to have quick and concise answers.',
+	'Try to answer in 2 to 3 sentences.',
+	'Answer like you would in a conversation, this is not a presentation.',
+];
